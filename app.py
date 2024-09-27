f7b91a1dee0e3c1e24a1d7e619570a9d8f0c6457
uzumakiN <shashank.yadav@kearney.com>
2024-09-27 11:02:29 +0530

    streamlit app

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np
+
+st.title("Hello Streamlit")
+
+st.write("This is a simple text")
+
+# create the df
+df = pd.DataFrame({
+    'first column' : [1,2,3,4],
+    'second column' : [10,20,30,40]
+})
+
+#display df
+st.write("Here is the dataframe")
+st.write(df)
+
+## create a line chart
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),columns=['a','b','c']
+)
+st.line_chart(chart_data)
+
